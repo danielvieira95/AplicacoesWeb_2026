588f3d53fac705a9b03bcbf514d3c6358e5e788d
danielvieira95 <engdanielvieira95@gmail.com>
2026-08-18 20:41:30 -0300

    fix Alteracao Charfield

--- Aula03/loja/settings.py
+++ Aula03/loja/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n^9fl6hajxgzt9apid*3jx0n73^^%_9&%g#sj7d+*pcml9q5h%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'produtos'
 ]
 
 MIDDLEWARE = [
