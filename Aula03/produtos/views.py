a21a573a65cba625fec97d63352f94509f8d9649
danielvieira95 <engdanielvieira95@gmail.com>
2026-09-08 20:53:45 -0300

    Aula 07 - Protegendo as rotas para fazer pedidos

--- Aula03/produtos/views.py
+++ Aula03/produtos/views.py
@@ -6,16 +6,32 @@ from rest_framework import mixins
 
 from .serializers import (ProdutoSerializer, CategoriaSerializer, ClienteSerializer, PedidoSerializer, ItemPedidoSerializer,StatusPedidoSerializer) 
 from .models import (Produto, Categoria, Cliente, Pedido, ItemPedido)  
-
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import(
     AllowAny, IsAuthenticated
 )
+
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import CadastroUsuarioSerializers
+
 # importando metodo para exibir uma pagina home
 
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Olá Django ! Aplicações Web 2026 -2 - Aula 05 Loja de Produtos")
+
+
+
+
+class CadastroUsuarioView(generics.CreateAPIView):
+    
+    queryset = User.objects.all()
+    serializer_class = CadastroUsuarioSerializers
+    permission_classes = [AllowAny]
+
 
     # Cria a classe Produtoviewset responsável por permitir fazer o crude
 
@@ -52,12 +68,7 @@ class ClienteViewSet(viewsets.ModelViewSet):
     serializer_class = ClienteSerializer
     
     
-# Pedido
-
-class PedidoViewSet(viewsets.ModelViewSet):
-    queryset = Pedido.objects.all().order_by("-id")
-    serializer_class = PedidoSerializer
-    
+   
     
 # ItemPedido
 
@@ -72,6 +83,31 @@ class ItemPedidoViewSet(viewsets.ModelViewSet):
 class PedidoViewSet(viewsets.ModelViewSet):
     queryset = Pedido.objects.all()
     serializer_class = PedidoSerializer
+    permission_classes = [IsAuthenticated]
+    
+    # Mostra somente os pedidos do usuario autenticado
+    
+    def get_queryset(self):
+        
+        return Pedido.objects.filter(
+            cliente_usuario = self.request.user).order_by("-id")
+        
+        
+    # Ao criar um pedido, associa automaticamente ao cliente autenticado
+    
+    def perform_create(self, serializer):
+        
+        try: 
+        
+             cliente = Cliente.objects.get(
+             usuario = self.request.user
+        )
+             
+        except Cliente.DoesNotExist:
+            raise ValidationError ("O usuario autenticado não possui um cliente associado")
+        
+        serializer.save(cliente =cliente)
+        
 
 
 
